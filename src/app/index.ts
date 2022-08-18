804f03052702d838db70c0e58d6b8b63a3860eb0
Siarhei <siarhei.by@gmail.com>
2022-08-18 20:07:39 +0300

    feat: setup environment

---
+++ src/app/index.ts
@@ -0,0 +1,7 @@
+class App {
+  async render() {
+    console.log('Hello from App');
+  }
+}
+
+export default App;
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import './style.scss';
+import App from './app/index';
+
+const app = new App();
+app.render();
