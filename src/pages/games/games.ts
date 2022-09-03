56347395a4015bfc33f8e7ced61007f93a199f11
Ihar Levachkov <pokerlis@gmail.com>
2022-09-03 22:18:20 +0300

    fix: fix style for games page

--- src/pages/games/games.ts
+++ src/pages/games/games.ts
@@ -42,7 +42,7 @@ export default class Games {
       ['game-audio__description'],
       'A word is voiced to you, and you choose the correct translation'
     ).render();
-    new BaseComponent(this.audioGame, 'button', ['game-audio__play'], 'play')
+    new BaseComponent(this.audioGame, 'button', ['game-audio__play'], 'start')
       .render()
       .addEventListener('click', ({ target }) => this.pushBtnPlayAudio(target as HTMLElement), { once: true });
 
@@ -53,9 +53,9 @@ export default class Games {
       this.sprintGame,
       'div',
       ['game-sprint__description'],
-      'In 10 seconds you have to answer the maximum number of correctly translated words'
+      'In 60 seconds you have to answer the maximum number of correctly translated words'
     ).render();
-    new BaseComponent(this.sprintGame, 'button', ['game-sprint__play'], 'play')
+    new BaseComponent(this.sprintGame, 'button', ['game-sprint__play'], 'start')
       .render()
       .addEventListener('click', ({ target }) => this.pushBtnPlaySprint(target as HTMLElement), { once: true });
 
